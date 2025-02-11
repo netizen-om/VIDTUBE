42bf68dcbe6ccad6124d566ebc5bac5642335cfc
netizen-om <borisagarom@gmail.com>
2025-02-11 21:45:46 +0530

    fix issues with comment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose"
-import {Comment} from "../models/comment.model.js"
+import { Comment } from "../models/comment.models.js"
 import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
+import ApiResponce from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
+//check onec
 const GetAllCommentsOfAVideo = syncHandler(async (req, res) => {
     const { videoId } = req.params
     const { page = 1, limit = 10 } = req.query;
@@ -103,6 +104,32 @@ const GetAllCommentsOfAVideo = syncHandler(async (req, res) => {
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    const { videoId } = req.params
+    const { content } = req.body
+
+    if(!content){
+        throw new ApiError(401, "content not found")
+    }
+
+    if(!videoId){
+        throw new ApiError(401, "Video not found")
+    }
+    
+    const comment = Comment.create({
+        video : videoId,
+        owner : req.user?._id,
+        content
+    })
+    
+    
+    if(!comment){
+        throw new ApiError(401, "Error while posting comment")
+    }
+
+    return res
+            .status(200)
+            .json(new ApiResponce(200,comment, "Comment created successfully"))
+
 })
 
 const updateComment = asyncHandler(async (req, res) => {
