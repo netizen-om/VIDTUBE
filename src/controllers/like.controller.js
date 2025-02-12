d326f27a220f66624bc6b2bc2b91c61f88f419ba
netizen-om <borisagarom@gmail.com>
2025-02-12 20:54:44 +0530

    toggle like complete

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,12 +1,38 @@
 import mongoose, {isValidObjectId} from "mongoose"
 import {Like} from "../models/like.model.js"
 import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
+import ApiResponce from "../utils/ApiResponse.js"
+import { Like } from "../models/like.models.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
     //TODO: toggle like on video
+    const {videoId} = req.params
+    if(!videoId){
+        return res
+                .status(401)
+                .json(new ApiError(401, "Video not found"))
+    }
+    
+    const isLikedVideo = await Like.findOne({ video : videoId, likedBy: req.user?._id })
+    if(isLikedVideo){
+        const deletedLike = await Like.findOneAndDelete({ video : videoId, likedBy: req.user?._id })
+        return res
+                .status(200)
+                .json(new ApiResponce(200, deletedLike, "Liked removed succesfully"))
+    } else {
+        const likedVideo = await Like.create({
+            video : videoId,
+            likedBy : req.user?._id
+        })
+        if(!likedVideo){
+            throw new ApiError(401, "Unable to Like the Video")
+        }
+
+        return res
+                .status(200)
+                .json(new ApiResponce("200", likedVideo, "Video Liked Successfully"))
+    }
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
