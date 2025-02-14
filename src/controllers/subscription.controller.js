eab97604dadb59c9b50c1a01d9e2de5a572d08b2
netizen-om <borisagarom@gmail.com>
2025-02-14 21:30:27 +0530

    add toggle Subscription

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,14 +1,48 @@
 import mongoose, {isValidObjectId} from "mongoose"
 import {User} from "../models/user.model.js"
-import { Subscription } from "../models/subscription.model.js"
+import { Subscription } from "../models/subscription.models.js"
 import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
+import ApiResponse from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+    if(!channelId) {
+        throw new ApiError(404, "Channel ID not found")
+    }
+    if(isValidObjectId(channelId)) {
+        throw new ApiError(400, "Channel ID is not valid")
+    }
+    
+    const isSub = await Subscription.findOne({ 
+        subscriber : req.user?._id,
+        channel : channelId 
+    })
+    
+    if(isSub) {
+        const toggleStatus = await Subscription.findByIdAndDelete(channelId)
+        if(!toggleStatus) {
+            throw new ApiError(400, "unable to toggle Subscription")
+        }
+        return res
+                .status(200)
+                .json(new ApiResponse(200, toggleStatus, "Subscription toggled"))
+    } else {
+        const toggleStatus = await Subscription.create({
+            subscriber : req.user?._id,
+            channel : channelId 
+        })
+
+        if(!toggleStatus) {
+            throw new ApiError(400, "unable to toggle Subscription")
+        }
+
+        return res
+                .status(200)
+                .json(new ApiResponse(200, toggleStatus, "Subscription toggled"))
+    }
 })
 
 // controller to return subscriber list of a channel
