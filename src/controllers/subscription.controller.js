210a2f3db300d9d20209410dbc294a8f3691e346
netizen-om <borisagarom@gmail.com>
2025-02-15 10:29:40 +0530

    add getUserChannelSubscribers

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -12,7 +12,7 @@ const toggleSubscription = asyncHandler(async (req, res) => {
     if(!channelId) {
         throw new ApiError(404, "Channel ID not found")
     }
-    if(isValidObjectId(channelId)) {
+    if(!isValidObjectId(channelId)) {
         throw new ApiError(400, "Channel ID is not valid")
     }
     
@@ -49,6 +49,50 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+    if(!channelId) {
+        throw new ApiError(404, "Channel ID not found")
+    }
+    if(!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Channel ID is not valid")
+    }
+
+    const subscribers = await Subscription.aggregate([
+        {
+            $match: { channel: new mongoose.Types.ObjectId(channelId) } // Match channel ID correctly
+        },
+        {
+            $lookup: {
+                from: "users", // Collection name of referenced users
+                localField: "subscriber",
+                foreignField: "_id",
+                as: "subscriberInfo"
+            }
+        },
+        {
+            $group: {
+                _id: "$channel", // Group by channelId
+                totalSubscribers: { $sum: 1 }, // Count number of subscribers
+                subscribers: { $push: "$subscriberInfo" } // Collect subscriber details
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                channelId: "$_id",
+                totalSubscribers: 1,
+                subscribers: {
+                    _id: 1,
+                    name: 1,
+                    email: 1
+                }
+            }
+        }
+    ]);
+    
+    return res
+            .status(200)
+            .json(new ApiResponse(200, subscribers[0] || { channelId, totalSubscribers: 0, subscribers: [] }, "Subscriber list fetched"));
+
 })
 
 // controller to return channel list to which user has subscribed
